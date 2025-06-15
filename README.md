5e7944089c6680dd738add25720929cfe81e7154
dgtlrst <makogn@proton.me>
2025-06-15 10:36:05 +0300

    a

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # bitcore
 
-[![Coverage](https://img.shields.io/badge/coverage-0%25-red)](https://github.com/Iskra-Initiative/bitcore/actions)
-[![License](https://img.shields.io/badge/license-MIT%20OR%20Apache--2.0-blue.svg)](LICENSE)
-
 - **TODO**
   - admin:
     - add license
--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,12 @@
 /// ```rust
 /// use bitcore::Serial;
 ///
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
 /// let serial = Serial::new("/dev/ttyUSB0")?;
 /// serial.write_str("hello")?;
 /// let response = serial.read_line()?;
-/// ```
+/// # Ok(())
+/// # }
 pub mod config;
 pub mod error;
 pub mod serial;
